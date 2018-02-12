bb1f71c9dbe24cb1edb073b4d545eecabc17ff6e
Fahran Wallace <me@fahran.net>
2018-02-12 12:36:15 +0000

    Uketool: fixes menu styling collapse. Fixes form for the autotweeter.

--- js/uke-love-machine.js
+++ js/uke-love-machine.js
@@ -1,5 +1,5 @@
 document.getElementById("ukulele-love-trigger").onclick = function (event) {
-    var key = document.getElementById("key").innerText;
+    var key = document.getElementById("key").value;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("POST", "https://vt9wuzbjg4.execute-api.us-east-1.amazonaws.com/prod/love", false ); // false for synchronous request
     xmlHttp.setRequestHeader("x-api-key", key);
